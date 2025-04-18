e81e07e120191b09106844074b5c9fd2c4cce9b4
codewithMihajlo <26mihajlo.simic@gmail.com>
2025-04-18 02:57:50 +0200

    Add files via upload

---
+++ js/Katalog.js
@@ -0,0 +1,98 @@
+
+/*function ispisiKatalog()
+{
+    fetch('Katalog.php')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('glavni_div').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error loading the content:', error);
+    });
+}*/
+function stavka1()
+{
+    var parametar = 1;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+
+function stavka2()
+{
+    var parametar = 2;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka3()
+{
+    var parametar = 3;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka4()
+{
+    var parametar = 4;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+
+function stavka5()
+{
+    var parametar = 5;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka6()
+{
+    var parametar = 6;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka7()
+{
+    var parametar = 7;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+
+function stavka8()
+{
+    var parametar = 8;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka9()
+{
+    var parametar = 9;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka10()
+{
+    var parametar = 10;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+
+function stavka11()
+{
+    var parametar = 11;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka12()
+{
+    var parametar = 12;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+
+function stavka13()
+{
+    var parametar = 13;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+
+function stavka14()
+{
+    var parametar = 14;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka15()
+{
+    var parametar = 15;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
+function stavka16()
+{
+    var parametar = 16;
+    window.location.href = "Katalog.php?katalog=" + parametar;
+}
